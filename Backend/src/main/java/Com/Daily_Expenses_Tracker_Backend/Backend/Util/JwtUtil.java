66894d792b1801c8704717bec305ea8939d04f58
DevHirusha <devhdrx354@gmail.com>
2026-09-15 20:53:19 +0530

    feat(auth): add JwtUtil for token generation

--- Backend/src/main/java/Com/Daily_Expenses_Tracker_Backend/Backend/Util/JwtUtil.java
+++ Backend/src/main/java/Com/Daily_Expenses_Tracker_Backend/Backend/Util/JwtUtil.java
@@ -13,21 +13,21 @@ import java.util.Map;
 @Component
 public class JwtUtil {
 
-    @Value("${jwt.secret.key}")
-      private String SECRET_KEY;
+  @Value("${jwt.secret.key}")
+  private String SECRET_KEY;
 
-    public String generateToken(UserDetails userDetails) {
-        Map<String, Object> claims = new HashMap<>();
-       return createToken(claims, userDetails.getUsername());
-    }
+  public String generateToken(UserDetails userDetails) {
+    Map<String, Object> claims = new HashMap<>();
+    return createToken(claims, userDetails.getUsername());
+  }
 
-    private String createToken(Map<String, Object> claims, String email) {
-       return Jwts.builder()
-                .setClaims(claims)
-                .setSubject(email)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) //10 hour expiration
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
-                .compact();
-    }
+  private String createToken(Map<String, Object> claims, String email) {
+    return Jwts.builder()
+        .setClaims(claims)
+        .setSubject(email)
+        .setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hour expiration
+        .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+        .compact();
+  }
 }
